bde8d7ade5798da0f904044c03f10cd13c1fdea5
Olga Yukhnik <olga.iukhnik@gmail.com>
2024-09-13 15:35:57 +0300

    junit4

--- src/test/java/CashbackHackServiceTest.java
+++ src/test/java/CashbackHackServiceTest.java
@@ -1,67 +1,66 @@
-import org.testng.annotations.Test;
 import ru.netology.CashbackHackService;
 
-import static org.testng.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemainIfAmountLess1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 500;
         int actual = cashbackHackService.remain(amount);
         int expected = 500;
 
-        assertEquals (actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemainIfAmount999() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 999;
         int actual = cashbackHackService.remain(amount);
         int expected = 1;
 
-        assertEquals (actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemainIf1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
         int actual = cashbackHackService.remain(amount);
         int expected = 0;
 
-        assertEquals (actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemainIfAmount1001() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1001;
         int actual = cashbackHackService.remain(amount);
         int expected = 999;
 
-        assertEquals (actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemainIfAmountMore1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1500;
         int actual = cashbackHackService.remain(amount);
         int expected = 500;
 
-        assertEquals (actual, expected);
+        assertEquals(expected, actual);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void shouldRemainIfAmount0() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 0;
         int actual = cashbackHackService.remain(amount);
         int expected = 1000;
 
-        assertEquals (actual, expected);
+        assertEquals(expected, actual);
     }
 }
